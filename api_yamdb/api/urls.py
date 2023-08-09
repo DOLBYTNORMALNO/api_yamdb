6ea5aabb19bca26467027c88714d5f97731aab0e
eldar-create <flotik1.1tonet@mail.ru>
2023-08-09 19:17:00 +0300

    добавлены роутеры

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+routers = DefaultRouter()
+routers.register(r'titles', TitleViewSet, basename='TitleSet')
+routers.register(r'genres', GenreViewSet, basename='GenreSet')
+routers.register(r'categories', CategoryViewSet, basename='CategorySet')
+
+
+
+
+urlpatterns = [
+    path('', include(routers.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+]
