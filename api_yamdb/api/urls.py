30c6440e0612463be14254d2b9644ff21da459e5
Alisher Ivazov <ali.ivazov@yandex.ru>
2023-08-10 22:32:36 +0300

    Добавлены урлы

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from .views import CommentViewSet, ReviewViewSet
+
 routers = DefaultRouter()
 routers.register(r'titles', TitleViewSet, basename='TitleSet')
 routers.register(r'genres', GenreViewSet, basename='GenreSet')
 routers.register(r'categories', CategoryViewSet, basename='CategorySet')
-
-
-
+routers.register(
+    r'titles/(?P<title_id>\d=)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='CommentSet'
+)
+routers.register(
+    r'titles/(?P<title_id>\d+)/reviews/',
+    ReviewViewSet,
+    basename='ReviewSet'
+)
 
 urlpatterns = [
     path('', include(routers.urls)),
