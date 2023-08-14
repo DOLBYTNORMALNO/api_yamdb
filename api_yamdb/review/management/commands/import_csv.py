a825c91a92712b237dec807da783dec74b4d8328
eldar-create <flotik1.1tonet@mail.ru>
2023-08-14 21:41:06 +0300

    Импорт файлов

---
+++ api_yamdb/review/management/commands/import_csv.py
@@ -0,0 +1,34 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from review.models import (Category, Comment, Genre, GenreTitle, Review,
+                            Title, CustomUser)
+
+#Словарь Модель+Файл.csv в БД 
+TABLES_DICT = {
+    CustomUser: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
+    GenreTitle: 'genre_title.csv'
+}
+
+
+class Command(BaseCommand):
+    #Загрузка данных
+    help = 'Load data from csv files'
+
+    def handle(self, *args, **kwargs):
+        for model, csv_file1 in TABLES_DICT.items():
+            with open(
+                f'{settings.BASE_DIR}/static/data/{csv_file1}',
+                'r', encoding='utf-8'
+            ) as csv_file:
+                reader = csv.DictReader(csv_file)
+                model.objects.bulk_create(model(**data) for data in reader)
+
+        self.stdout.write(self.style.SUCCESS('Successfully load data'))
