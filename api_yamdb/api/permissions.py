d96ed92f0e22a06d953bcb6822bf479b8d71c7db
Григорий Серов <grisha200170@gmail.com>
2023-08-19 16:27:10 +0300

    Before merge(no changes)

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,13 +10,6 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
         return request.user.is_authenticated and request.user.role == 'admin'
 
-class IsAmdinOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user in permissions.SAFE_METHODS
-            or request.user.is_staff or request.user.role == 'admin'
-        )
-
 
 class IsAdmin(permissions.BasePermission):
     """
