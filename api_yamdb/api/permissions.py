d36dc8e4e47dca89f86145021e1d118432d82ef8
Григорий Серов <grisha200170@gmail.com>
2023-08-29 15:47:31 +0300

    pep8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,16 +4,16 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_authenticated and request.user.is_admin
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated and request.user.is_admin)
         )
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.user.is_authenticated and
-            (request.user.is_admin or request.user.is_superuser)
+            request.user.is_authenticated
+            and (request.user.is_admin or request.user.is_superuser)
         )
 
 
@@ -25,18 +25,18 @@ class IsSelfOrAdmin(permissions.BasePermission):
 class IsAuthenticatedOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
 
 class IsAuthorOrModeratorOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
-            request.user.is_authenticated and
-            (
-                obj.author == request.user or
-                request.user.is_moderator or
-                request.user.is_admin
+            request.user.is_authenticated
+            and (
+                obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin
             )
         )
